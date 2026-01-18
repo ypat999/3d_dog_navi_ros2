fe3e12b8f2a9364e53499b5121ae2fad79473a33
ypat999 <ypat999@gmail.com>
2026-01-18 15:03:42 +0800

    sync

--- src/FAST_LIO_ROS2_edit/launch/mapping.launch.py
+++ src/FAST_LIO_ROS2_edit/launch/mapping.launch.py
@@ -99,15 +99,15 @@ def generate_launch_description():
     ld.add_action(static_transform_map_to_odom)
 
     # odom -> base_link (里程计到机器人基坐标系的静态变换)
-    static_transform_odom_to_base_link = Node(
+    static_transform_odom_to_base = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        name='static_transform_odom_to_base_link',
+        name='static_transform_odom_to_base',
         parameters=[{'use_sim_time': True}],
-        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'odom', 'base_link'],
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'odom', 'base'],
         output='screen'
     )
-    ld.add_action(static_transform_odom_to_base_link)
+    # ld.add_action(static_transform_odom_to_base)
 
 
     return ld
